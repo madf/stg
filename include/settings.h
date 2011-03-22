be43a75f20a1250f61f9cfc46ba5b25177cd8024
Maxim Mamontov <faust.madf@gmail.com>
2011-03-22 14:05:02 +0200

    Add settings interface

---
+++ include/settings.h
@@ -0,0 +1,45 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ *    Author : Maxim Mamontov <faust@stargazer.dp.ua>
+ */
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <string>
+
+class SETTINGS {
+public:
+    virtual const std::string & GetDirName(size_t num) const = 0;
+    virtual unsigned            GetDetailStatWritePeriod() const = 0;
+    virtual unsigned            GetStatWritePeriod() const = 0;
+    virtual unsigned            GetDayFee() const = 0;
+    virtual bool                GetFullFee() const = 0;
+    virtual unsigned            GetDayResetTraff() const = 0;
+    virtual bool                GetSpreadFee() const = 0;
+    virtual bool                GetFreeMbAllowInet() const = 0;
+    virtual bool                GetDayFeeIsLastDay() const = 0;
+    virtual bool                GetWriteFreeMbTraffCost() const = 0;
+    virtual bool                GetShowFeeInCash() const = 0;
+    virtual unsigned            GetMessageTimeout() const = 0;
+    virtual const std::string & GetMonitorDir() const = 0;
+    virtual bool                GetMonitoring() const = 0;
+};
+//-----------------------------------------------------------------------------
+
+#endif
