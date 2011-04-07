5e6957abc520ebcba78b1bf5fccd5f257a1bd0a5
Maxim Mamontov <faust@gts.dp.ua>
2011-04-07 18:38:49 +0300

    Fix ipq capture plugin compilation errors

--- projects/stargazer/plugins/capture/ipq_linux/ipq_cap.cpp
+++ projects/stargazer/plugins/capture/ipq_linux/ipq_cap.cpp
@@ -1,11 +1,31 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+* Author : Boris Mikhailenko <stg34@stargazer.dp.ua>
+*/
+
 #include <signal.h>
 #include <cerrno>
 #include <netinet/in.h>
 #include <linux/netfilter.h>
 
+#include "stg/raw_ip_packet.h"
+#include "stg/traffcounter.h"
 #include "ipq_cap.h"
-#include "raw_ip_packet.h"
-#include "traffcounter.h"
 
 extern "C"
 {
