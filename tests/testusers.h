25e5ba1bdfefce82ff8e4bb0221de19a538b4d3c
Maxim Mamontov <faust.madf@gmail.com>
2011-05-28 17:36:51 +0300

    Split lines to fit 80 char width

--- tests/testusers.h
+++ tests/testusers.h
@@ -5,9 +5,11 @@ class TEST_USERS : public USERS {
     public:
         TEST_USERS() {}
 
-        int  FindByName(const std::string & /*login*/, USER_PTR * /*user*/) { return -1; }
+        int  FindByName(const std::string & /*login*/, USER_PTR * /*user*/)
+        { return -1; }
 
-        bool TariffInUse(const std::string & /*tariffName*/) const { return -1; }
+        bool TariffInUse(const std::string & /*tariffName*/) const
+        { return -1; }
 
         void AddNotifierUserAdd(NOTIFIER_BASE<USER_PTR> * /*notifier*/) {}
         void DelNotifierUserAdd(NOTIFIER_BASE<USER_PTR> * /*notifier*/) {}
@@ -15,13 +17,15 @@ class TEST_USERS : public USERS {
         void AddNotifierUserDel(NOTIFIER_BASE<USER_PTR> * /*notifier*/) {}
         void DelNotifierUserDel(NOTIFIER_BASE<USER_PTR> * /*notifier*/) {}
 
-        int  Add(const std::string & /*login*/, const ADMIN * /*admin*/) { return 0; }
+        int  Add(const std::string & /*login*/, const ADMIN * /*admin*/)
+        { return 0; }
         void Del(const std::string & /*login*/, const ADMIN * /*admin*/) {}
 
         int  ReadUsers() { return 0; }
         int  GetUserNum() const { return 0; }
 
-        int  FindByIPIdx(uint32_t /*ip*/, USER_PTR * /*user*/) const { return -1; }
+        int  FindByIPIdx(uint32_t /*ip*/, USER_PTR * /*user*/) const
+        { return -1; }
         bool IsIPInIndex(uint32_t /*ip*/) const { return false; }
 
         int  OpenSearch() { return 0; }
