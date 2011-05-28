9f01f72d7b1da24b97bcfed87a41f37fd88e7d10
Maxim Mamontov <faust.madf@gmail.com>
2011-05-28 17:34:39 +0300

    TEST_SETTINGS_LOCAL moved outside the namespace, more assertions added

--- tests/test_reconnect_on_tariff_change.cpp
+++ tests/test_reconnect_on_tariff_change.cpp
@@ -27,6 +27,19 @@ private:
     size_t disconnects;
 };
 
+class TEST_SETTINGS_LOCAL : public TEST_SETTINGS {
+    public:
+        TEST_SETTINGS_LOCAL(bool _reconnectOnTariffChange)
+            : TEST_SETTINGS(),
+              reconnectOnTariffChange(_reconnectOnTariffChange)
+        {}
+
+        bool GetReconnectOnTariffChange() const { return reconnectOnTariffChange; }
+
+    private:
+        bool reconnectOnTariffChange;
+};
+
 namespace tut
 {
     struct reconnect_on_tariff_change_data {
@@ -36,18 +49,6 @@ namespace tut
     tg reconnect_on_tariff_change_test_group("Reconnect on tariff change tests group");
 
     typedef tg::object testobject;
-
-    class TEST_SETTINGS_LOCAL : public TEST_SETTINGS {
-        public:
-            TEST_SETTINGS_LOCAL(bool _reconnectOnTariffChange)
-                : reconnectOnTariffChange(_reconnectOnTariffChange)
-            {}
-
-            bool GetReconnectOnTariffChange() const { return reconnectOnTariffChange; }
-
-        private:
-            bool reconnectOnTariffChange;
-    };
 
     template<>
     template<>
@@ -79,16 +80,81 @@ namespace tut
 
         ensure_equals("user.tariffName == NO_TARIFF_NAME", user.GetProperty().tariffName.ConstData(), NO_TARIFF_NAME);
 
-        tariffName = "test";
-        ensure_equals("user.tariffName == 'test'", user.GetProperty().tariffName.ConstData(), "test");
-
         user.Authorize(inet_strington("127.0.0.1"), 0, &auth);
+        user.Run();
 
         ensure_equals("user.authorised_by = true", user.IsAuthorizedBy(&auth), true);
 
         ensure_equals("user.connected = true", user.GetConnected(), true);
         ensure_equals("connects = 1", connectionNotifier.GetConnects(), 1);
         ensure_equals("disconnects = 0", connectionNotifier.GetDisconnects(), 0);
+
+        tariffName = "test";
+        ensure_equals("user.tariffName == 'test'", user.GetProperty().tariffName.ConstData(), "test");
+
+        ensure_equals("user.authorised_by = true", user.IsAuthorizedBy(&auth), true);
+
+        ensure_equals("user.connected = true", user.GetConnected(), true);
+        ensure_equals("connects = 1", connectionNotifier.GetConnects(), 1);
+        ensure_equals("disconnects = 0", connectionNotifier.GetDisconnects(), 0);
+    }
+
+
+    template<>
+    template<>
+    void testobject::test<2>()
+    {
+        set_test_name("Check reconnect on tariff change");
+
+        TEST_SETTINGS_LOCAL settings(true);
+
+        TEST_SETTINGS * s1 = &settings;
+        SETTINGS * s2 = &settings;
+
+        ensure("settings.GetReconnectOnTariffChange() == true", settings.GetReconnectOnTariffChange());
+        ensure("s1->GetReconnectOnTariffChange() == true", s1->GetReconnectOnTariffChange());
+        ensure("s2->GetReconnectOnTariffChange() == true", s2->GetReconnectOnTariffChange());
+
+        TEST_TARIFFS tariffs;
+        TEST_ADMIN admin;
+        TEST_STORE store;
+        TEST_AUTH auth;
+        TEST_USERS users;
+        USER_IMPL user(&settings, &store, &tariffs, &admin, &users);
+
+        AFTER_CONNECTED_NOTIFIER connectionNotifier;
+
+        user.AddConnectedAfterNotifier(&connectionNotifier);
+
+        USER_PROPERTY<double> & cash(user.GetProperty().cash);
+        USER_PROPERTY<std::string> & tariffName(user.GetProperty().tariffName);
+        USER_PROPERTY<USER_IPS> & ips(user.GetProperty().ips);
+
+        ips = StrToIPS("*");
+
+        ensure_equals("user.connected = false", user.GetConnected(), false);
+        ensure_equals("connects = 0", connectionNotifier.GetConnects(), 0);
+        ensure_equals("disconnects = 0", connectionNotifier.GetDisconnects(), 0);
+
+        ensure_equals("user.tariffName == NO_TARIFF_NAME", user.GetProperty().tariffName.ConstData(), NO_TARIFF_NAME);
+
+        user.Authorize(inet_strington("127.0.0.1"), 0, &auth);
+        user.Run();
+
+        ensure_equals("user.authorised_by = true", user.IsAuthorizedBy(&auth), true);
+
+        ensure_equals("user.connected = true", user.GetConnected(), true);
+        ensure_equals("connects = 1", connectionNotifier.GetConnects(), 1);
+        ensure_equals("disconnects = 0", connectionNotifier.GetDisconnects(), 0);
+
+        tariffName = "test";
+        ensure_equals("user.tariffName == 'test'", user.GetProperty().tariffName.ConstData(), "test");
+
+        ensure_equals("user.authorised_by = true", user.IsAuthorizedBy(&auth), true);
+
+        ensure_equals("user.connected = true", user.GetConnected(), true);
+        ensure_equals("connects = 2", connectionNotifier.GetConnects(), 2);
+        ensure_equals("disconnects = 1", connectionNotifier.GetDisconnects(), 1);
     }
 }
 
