a5ce0ca3674081d3b761eb1bc88654d6fd9d3755
Maxim Mamontov <faust.madf@gmail.com>
2011-08-13 15:42:04 +0300

    Fix fild types in services and corporations

--- include/stg/corp_conf.h
+++ include/stg/corp_conf.h
@@ -1,10 +1,15 @@
 #ifndef CORP_CONF_H
 #define CORP_CONF_H
 
+#include <string>
+
 struct CORP_CONF
 {
-string  name;
-double  cash;
+CORP_CONF(const std::string & n) : name(n), cash(0) {}
+CORP_CONF(const std::string & n, double c) : name(n), cash(c) {}
+
+std::string name;
+double      cash;
 };
 
 #endif //CORP_CONF_H
--- include/stg/service_conf.h
+++ include/stg/service_conf.h
@@ -1,12 +1,28 @@
 #ifndef SERVICE_CONF_H
 #define SERVICE_CONF_H
 
+#include <string>
+
 struct SERVICE_CONF
 {
-string  name;
-string  comment;
-double  cost;
-int     payDay;
+SERVICE_CONF(const std::string & n)
+    : name(n), comment(), cost(0), payDay(0)
+{}
+SERVICE_CONF(const std::string & n, double c)
+    : name(n), comment(), cost(c), payDay(0)
+{}
+SERVICE_CONF(const std::string & n, double c, unsigned p)
+    : name(n), comment(), cost(c), payDay(p)
+{}
+SERVICE_CONF(const std::string & n, double c,
+             unsigned p, const std::string & com)
+    : name(n), comment(com), cost(c), payDay(p)
+{}
+
+std::string name;
+std::string comment;
+double      cost;
+unsigned    payDay;
 };
 
 #endif //SERVICE_CONF_H
