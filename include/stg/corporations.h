305dd917d2ba383e8c468c006d1d75edbc705337
Maxim Mamontov <faust.madf@gmail.com>
2011-08-13 15:41:07 +0300

    Services and coroprations interface added

---
+++ include/stg/corporations.h
@@ -0,0 +1,42 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ *    Author : Maxim Mamontov <faust@stargazer.dp.ua>
+ */
+
+#ifndef CORPORATIONS_H
+#define CORPORATIONS_H
+
+#include <string>
+
+#include "corp_conf.h"
+
+class CORPORATIONS {
+public:
+    virtual int Add(const CORP_CONF & corp) = 0;
+    virtual int Del(const std::string & name) = 0;
+    virtual int Change(const CORP_CONF & corp) = 0;
+    virtual bool FindCorp(const std::string & name, CORP_CONF & corp) = 0;
+    virtual bool CorpExists(const std::string & name) const = 0;
+    virtual const std::string & GetStrError() const = 0;
+
+    virtual int OpenSearch() const = 0;
+    virtual int SearchNext(int, CORP_CONF * corp) const = 0;
+    virtual int CloseSearch(int) const = 0;
+};
+
+#endif
---
+++ include/stg/services.h
@@ -0,0 +1,42 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ *    Author : Maxim Mamontov <faust@stargazer.dp.ua>
+ */
+
+#ifndef SERVICES_H
+#define SERVICES_H
+
+#include <string>
+
+#include "service_conf.h"
+
+class SERVICES {
+public:
+    virtual int Add(const SERVICE_CONF & service) = 0;
+    virtual int Del(const std::string & name) = 0;
+    virtual int Change(const SERVICE_CONF & service) = 0;
+    virtual bool FindService(const std::string & name, SERVICE_CONF & service) = 0;
+    virtual bool ServiceExists(const std::string & name) const = 0;
+    virtual const std::string & GetStrError() const = 0;
+
+    virtual int OpenSearch() const = 0;
+    virtual int SearchNext(int, SERVICE_CONF * service) const = 0;
+    virtual int CloseSearch(int) const = 0;
+};
+
+#endif
