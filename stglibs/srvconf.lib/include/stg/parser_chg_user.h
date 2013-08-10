02d8ff35c9a854ffd15a701e46a118bc51be3b7d
Maxim Mamontov <faust.madf@gmail.com>
2013-08-10 22:56:09 +0300

    Added setter.

---
+++ stglibs/srvconf.lib/include/stg/parser_chg_user.h
@@ -0,0 +1,42 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ *    Author : Boris Mikhailenko <stg34@stargazer.dp.ua>
+ *    Author : Maxim Mamontov <faust@stargazer.dp.ua>
+ */
+
+#ifndef __STG_STGLIBS_SRVCONF_PARSER_CHG_USER_H__
+#define __STG_STGLIBS_SRVCONF_PARSER_CHG_USER_H__
+
+#include "parser.h"
+
+class PARSER_CHG_USER: public PARSER
+{
+public:
+    PARSER_CHG_USER();
+    int  ParseStart(const char *el, const char **attr);
+    void ParseEnd(const char *el);
+    void ParseAnswer(const char *el, const char **attr);
+    void SetChgUserRecvCb(RecvChgUserCb_t, void * data);
+private:
+    RecvChgUserCb_t RecvChgUserCb;
+    void * chgUserCbData;
+    int depth;
+    bool error;
+};
+
+#endif
