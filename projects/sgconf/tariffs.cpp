81b359cdaff64a514ddfff9f86e398d0d37c06d1
Maxim Mamontov <faust.madf@gmail.com>
2014-05-30 10:11:03 +0300

    Fixed typo.

--- projects/sgconf/tariffs.cpp
+++ projects/sgconf/tariffs.cpp
@@ -235,7 +235,7 @@ params.push_back(SGCONF::API_ACTION::PARAM("traff-type", "<type>", "\ttraffic ty
 params.push_back(SGCONF::API_ACTION::PARAM("period", "<period>", "\ttarification period (daily, monthly)"));
 params.push_back(SGCONF::API_ACTION::PARAM("times", "<hh:mm-hh:mm, ...>", "coma-separated day time-spans for each direction"));
 params.push_back(SGCONF::API_ACTION::PARAM("day-prices", "<price/price, ...>", "coma-separated day prices for each direction"));
-params.push_back(SGCONF::API_ACTION::PARAM("night-prices", "<price/price, ...>", "coma-separated day prices for each direction"));
+params.push_back(SGCONF::API_ACTION::PARAM("night-prices", "<price/price, ...>", "coma-separated night prices for each direction"));
 params.push_back(SGCONF::API_ACTION::PARAM("thresholds", "<threshold, ...>", "coma-separated thresholds for each direction"));
 return params;
 }
