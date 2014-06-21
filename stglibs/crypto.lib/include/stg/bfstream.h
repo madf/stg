27fd26be047af5ccd9d023cf0134fcbf272f18cd
Maxim Mamontov <faust.madf@gmail.com>
2014-06-21 23:39:36 +0300

    Added stream encryption/decryption.

---
+++ stglibs/crypto.lib/include/stg/bfstream.h
@@ -0,0 +1,42 @@
+#ifndef __STG_STGLIBS_BF_STREAM_H__
+#define __STG_STGLIBS_BF_STREAM_H__
+
+#include <string>
+#include <cstddef> // size_t
+
+namespace STG
+{
+
+class ENCRYPT_STREAM
+{
+    public:
+        typedef void (* CALLBACK)(const void * block, size_t size, void * data);
+
+        ENCRYPT_STREAM(const std::string & key, CALLBACK callback, void * data);
+        ~ENCRYPT_STREAM();
+        void Put(const void * data, size_t size, bool last = false);
+
+    private:
+        class IMPL;
+
+        IMPL * m_impl;
+};
+
+class DECRYPT_STREAM
+{
+    public:
+        typedef void (* CALLBACK)(const void * block, size_t size, void * data);
+
+        DECRYPT_STREAM(const std::string & key, CALLBACK callback, void * data);
+        ~DECRYPT_STREAM();
+        void Put(const void * data, size_t size, bool last = false);
+
+    private:
+        class IMPL;
+
+        IMPL * m_impl;
+};
+
+} // namespace STG
+
+#endif
