79c8c4ef19419fd58a7aab7efd6a299b295e55b3
Elena Mamontova <helenh463@gmail.com>
2016-09-06 15:06:25 +0300

    Ticket 37. The testobject::test<9>() function added.

--- tests/test_tariff.cpp
+++ tests/test_tariff.cpp
@@ -398,4 +398,31 @@ namespace tut
 
         ensure_equals("Allow expensive", !tariff.TariffChangeIsAllowed(expensive).empty(), true);
     }
+
+    template<>
+    template<>
+    void testobject::test<9>()
+    {
+        set_test_name("Check changePolicy - TO_EXPENSIVE");
+
+        TARIFF_DATA td("test");
+        td.tariffConf.changePolicy = TARIFF::TO_EXPENSIVE;
+        td.tariffConf.fee = 100;
+        TARIFF_IMPL tariff(td);
+
+        td.tariffConf.fee = 50;
+        TARIFF_IMPL cheaper(td);
+
+        ensure_equals("Allow cheaper", !tariff.TariffChangeIsAllowed(cheaper).empty(), true);
+
+        td.tariffConf.fee = 100;
+        TARIFF_IMPL equals(td);
+
+        ensure_equals("Allow equal", !tariff.TariffChangeIsAllowed(equals).empty(), true);
+
+        td.tariffConf.fee = 150;
+        TARIFF_IMPL expensive(td);
+
+        ensure_equals("Allow expensive", tariff.TariffChangeIsAllowed(expensive).empty(), true);
+    }
 }
