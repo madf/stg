0f56d028b335f16e9c2a8af0fff90048f6783d0a
Elena Mamontova <helenh463@gmail.com>
2016-09-09 10:27:17 +0300

    Ticket 37. 'Allow equal', 'Allow cheaper' and 'Allow expensive' comments changed to 'Prohibit equal', 'Prohibit cheaper' and 'Prohibit expensive' in the testobject::test<10>() function.

--- tests/test_tariff.cpp
+++ tests/test_tariff.cpp
@@ -440,16 +440,16 @@ namespace tut
         td.tariffConf.fee = 50;
         TARIFF_IMPL cheaper(td);
 
-        ensure_equals("Allow cheaper", !tariff.TariffChangeIsAllowed(cheaper).empty(), true);
+        ensure_equals("Prohibit cheaper", !tariff.TariffChangeIsAllowed(cheaper).empty(), true);
 
         td.tariffConf.fee = 100;
         TARIFF_IMPL equal(td);
 
-        ensure_equals("Allow equal", !tariff.TariffChangeIsAllowed(equal).empty(), true);
+        ensure_equals("Prohibit equal", !tariff.TariffChangeIsAllowed(equal).empty(), true);
 
         td.tariffConf.fee = 150;
         TARIFF_IMPL expensive(td);
 
-        ensure_equals("Allow expensive", !tariff.TariffChangeIsAllowed(expensive).empty(), true);
+        ensure_equals("Prohibit expensive", !tariff.TariffChangeIsAllowed(expensive).empty(), true);
     }
 }
