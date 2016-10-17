16ccac1f88220ea4be19ebcebd925de077e52f60
Elena Mamontova <helenh463@gmail.com>
2016-10-17 10:30:01 +0300

    Ticket 37. The 'change_policy_timeout' field added. DROP DEFAULT added for the 'change_policy', 'change_policy_timeout'. UPDATE added for the 'change_policy', 'change_policy_timeout'.

--- projects/stargazer/inst/var/02-alter-03.sql
+++ projects/stargazer/inst/var/02-alter-03.sql
@@ -6,5 +6,11 @@ CREATE DOMAIN DM_TARIFF_CHANGE_POLICY AS  VARCHAR(32) NOT NULL
     CHECK (VALUE IN ('allow', 'to_cheap', 'to_expensive', 'deny'));
 
 ALTER TABLE tb_tariffs ADD change_policy DM_TARIFF_CHANGE_POLICY DEFAULT 'allow';
+ALTER TABLE tb_tariffs ADD change_policy_timeout DM_MOMENT DEFAULT '1970-01-01 00:00:00';
+
+ALTER TABLE tb_tariffs ALTER COLUMN change_policy DROP DEFAULT;
+ALTER TABLE tb_tariffs ALTER COLUMN change_policy_timeout DROP DEFAULT;
+
+UPDATE tb_tariffs SET change_policy = 'allow', change_policy_timeout = '1970-01-01 00:00:00';
 
 UPDATE tb_info SET version = 2;
