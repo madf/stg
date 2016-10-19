c2c22250c1f6fe2ed787b96ac6ff7dae96f44464
Elena Mamontova <helenh463@gmail.com>
2016-10-19 10:24:45 +0300

    Ticket 37. UPDATE moved before DROP DEFAULT.

--- projects/stargazer/inst/var/02-alter-03.sql
+++ projects/stargazer/inst/var/02-alter-03.sql
@@ -8,9 +8,9 @@ CREATE DOMAIN DM_TARIFF_CHANGE_POLICY AS  VARCHAR(32) NOT NULL
 ALTER TABLE tb_tariffs ADD change_policy DM_TARIFF_CHANGE_POLICY DEFAULT 'allow';
 ALTER TABLE tb_tariffs ADD change_policy_timeout DM_MOMENT DEFAULT '1970-01-01 00:00:00';
 
+UPDATE tb_tariffs SET change_policy = 'allow', change_policy_timeout = '1970-01-01 00:00:00';
+
 ALTER TABLE tb_tariffs ALTER COLUMN change_policy DROP DEFAULT;
 ALTER TABLE tb_tariffs ALTER COLUMN change_policy_timeout DROP DEFAULT;
-
-UPDATE tb_tariffs SET change_policy = 'allow', change_policy_timeout = '1970-01-01 00:00:00';
 
 UPDATE tb_info SET version = 2;
