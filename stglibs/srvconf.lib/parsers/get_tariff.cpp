89f44c382a46aeae56918ccae49d528543518698
Elena Mamontova <helenh463@gmail.com>
2016-11-01 16:11:01 +0200

    Ticket. AddParser() call added for the changePolicyTimeout.

--- stglibs/srvconf.lib/parsers/get_tariff.cpp
+++ stglibs/srvconf.lib/parsers/get_tariff.cpp
@@ -160,6 +160,7 @@ GET_TARIFF::PARSER::PARSER(CALLBACK f, void * d, const std::string & e)
     AddParser(propertyParsers, "traffType", info.tariffConf.traffType, GetTraffType);
     AddParser(propertyParsers, "period", info.tariffConf.period, GetPeriod);
     AddParser(propertyParsers, "changePolicy", info.tariffConf.changePolicy, GetChangePolicy);
+    AddParser(propertyParsers, "changePolicyTimeout", info.tariffConf.changePolicyTimeout, GetChangePolicyTimeout);
     for (size_t i = 0; i < DIR_NUM; ++i)
         AddParser(propertyParsers, "time" + unsigned2str(i), info.dirPrice[i], GetTimeSpan);
     AddAOSParser(propertyParsers, "priceDayA", info.dirPrice, &DIRPRICE_DATA::priceDayA, GetSlashedValue);
