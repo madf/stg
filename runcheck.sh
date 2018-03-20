d2fc992c13b7684957b4bfa4b409389e9383d333
Maxim Mamontov <madf@madf.info>
2018-03-20 12:23:16 +0200

    Add a script to run cppcheck over all projects.

---
+++ runcheck.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+INCLUDES="-I include/
+          -I stglibs/common.lib/include/
+          -I stglibs/conffiles.lib/include/
+          -I stglibs/crypto.lib/include/
+          -I stglibs/dotconfpp.lib/include/
+          -I stglibs/ia.lib/include/
+          -I stglibs/ibpp.lib/include/
+          -I stglibs/logger.lib/include/
+          -I stglibs/pinger.lib/include/
+          -I stglibs/scriptexecuter.lib/include/
+          -I stglibs/smux.lib/include/
+          -I stglibs/srvconf.lib/include/"
+
+PROJECTS="projects/libs
+          projects/rscriptd
+          projects/sgauth
+          projects/sgconf
+          projects/sgconf_xml
+          projects/sgconv
+          projects/stargazer"
+
+cppcheck ${INCLUDES} --enable=all -q ${PROJECTS}
