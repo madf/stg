c8e999732ad238940f3384be617989a8de226e90
Elena Mamontova <helenh463@gmail.com>
2024-11-19 16:56:26 +0200

    Header file <cassert> added. Definition methods GetUser, AddUser, DelUser, UpdateUserAuthorization added.

--- projects/stargazer/plugins/other/radius/radius.cpp
+++ projects/stargazer/plugins/other/radius/radius.cpp
@@ -3,6 +3,7 @@
 #include "stg/users.h"
 
 #include <functional> // mem_fun_ref
+#include <cassert>
 
 
 using STG::RADIUS;
@@ -52,8 +53,47 @@ int RADIUS::Stop()
     return 0;
 }
 
+void RADIUS::GetUsers()
+{
+    UserPtr u;
+    int h = users->OpenSearch();
+    assert(h && "USERS::OpenSearch is always correct");
 
+    while (!users->SearchNext(h, &u))
+        {
+            userList.push_back(u);
+            SetUserNotifiers(u);
+        }
 
+    users->CloseSearch(h);
+}
+
+void RADIUS::UpdateUserAuthorization(ConstUserPtr u) const
+{
+    if (u->GetProperties().alwaysOnline)
+    {
+        auto ips = u->GetProperties().ips.get();
+        if (ips.onlyOneIP())
+        {
+            users->Authorize(u->GetLogin(), ips[0].ip, 0xFFffFFff, this);
+        }
+    }
+}
+
+void RADIUS::AddUser(UserPtr u)
+{
+    SetUserNotifiers(u);
+    userList.push_back(u);
+    UpdateUserAuthorization(u);
+}
+
+void RADIUS::DelUser(UserPtr u)
+{
+    if (u->IsAuthorizedBy(this))
+        users->Unauthorize(u->GetLogin(), this);
+    UnSetUserNotifiers(u);
+    userList.erase(std::remove(userList.begin(), userList.end(), u), userList.end());
+}
 
 int RADIUS::SendMessage(const Message &, uint32_t) const
 {
