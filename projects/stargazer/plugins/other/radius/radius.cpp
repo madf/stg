f6f527d1cd4ce14b9e74b259f2655ca6cc3f9b34
Elena Mamontova <helenh463@gmail.com>
2024-11-19 16:00:01 +0200

    Header files <functional>, "stg/user.h" added. Overriding virtual methods Start, Stop added.

--- projects/stargazer/plugins/other/radius/radius.cpp
+++ projects/stargazer/plugins/other/radius/radius.cpp
@@ -1,5 +1,8 @@
 #include "radius.h"
+#include "stg/user.h"
 #include "stg/users.h"
+
+#include <functional> // mem_fun_ref
 
 
 using STG::RADIUS;
@@ -12,16 +15,49 @@ extern "C" STG::Plugin* GetPlugin()
 
 std::string RADIUS::GetVersion() const
 {
-return "Radius authorizator v.1.0";
+    return "Radius authorizator v.1.0";
 }
 
 RADIUS::RADIUS()
 {
 }
 
+int RADIUS::Start()
+{
+    printfd(__FILE__, "RADIUS::Start()\n");
+    GetUsers();
+
+    m_onAddUserConn = users->onAdd([this](auto user){ AddUser(user); });
+    m_onDelUserConn = users->onDel([this](auto user){ DelUser(user); });
+
+    std::for_each(userList.begin(), userList.end(), [this](auto user){ UpdateUserAuthorization(user); });
+
+    isRunning = true;
+
+    return 0;
+}
+
+int RADIUS::Stop()
+{
+    printfd(__FILE__, "RADIUS::Stop()\n");
+    if (!isRunning)
+        return 0;
+
+    m_onAddUserConn.disconnect();
+    m_onDelUserConn.disconnect();
+
+    m_conns.clear();
+
+    isRunning = false;
+    return 0;
+}
+
+
+
+
 int RADIUS::SendMessage(const Message &, uint32_t) const
 {
-errorStr = "Authorization modele \'Radius\' does not support sending messages";
-return -1;
+    errorStr = "Authorization modele \'Radius\' does not support sending messages";
+    return -1;
 }
 
