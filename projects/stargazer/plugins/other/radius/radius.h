acdce4385ffa34e04460254f2c17c76f39c9c8bb
Elena Mamontova <helenh463@gmail.com>
2024-11-20 16:06:43 +0200

    Overriding virtual methods Start, Stop, Reload, IsRunning, ParseSettings, GetStrErroor, GetStartPosition, GetStopPosition, SendMessage added. Class member variable errorStr, isRunning added.

--- projects/stargazer/plugins/other/radius/radius.h
+++ projects/stargazer/plugins/other/radius/radius.h
@@ -1,23 +1,30 @@
 #pragma once
 
 #include "stg/auth.h"
-
 #include <string>
 
 namespace STG
 {
-
     class RADIUS : public Auth
     {
         public:
             RADIUS();
 
-            int Start() override;
-            int Stop() override;
+            int Start() override { return 0; }
+            int Stop() override { return 0; }
+            int Reload(const ModuleSettings & /*ms*/) override { return 0; }
+            bool IsRunning() override { return isRunning; }
+            int ParseSettings() override { return 0; }
+            const std::string & GetStrError() const override { return errorStr; }
             std::string GetVersion() const override;
+            uint16_t GetStartPosition() const override { return 0; }
+            uint16_t GetStopPosition() const override { return 0; }
 
-            int SendMessage(const Message & msg, uint32_t ip) const override;
+            int SendMessage(const Message & msg, uint32_t ip) const override { return 0; }
+
+        private:
+            mutable std::string errorStr;
+            bool isRunning;
 
     };
-
 }
