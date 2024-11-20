d9dc70b9920fcac2fa0f54560866cd0f8d3ac39b
Elena Mamontova <helenh463@gmail.com>
2024-11-20 16:19:23 +0200

    Definition of methods Start, Stop, SendMessage removed.

--- projects/stargazer/plugins/other/radius/radius.cpp
+++ projects/stargazer/plugins/other/radius/radius.cpp
@@ -17,39 +17,3 @@ RADIUS::RADIUS()
 {
 }
 
-int RADIUS::Start()
-{
-    printfd(__FILE__, "RADIUS::Start()\n");
-    GetUsers();
-
-    m_onAddUserConn = users->onAdd([this](auto user){ AddUser(user); });
-    m_onDelUserConn = users->onDel([this](auto user){ DelUser(user); });
-
-    std::for_each(userList.begin(), userList.end(), [this](auto user){ UpdateUserAuthorization(user); });
-
-    isRunning = true;
-
-    return 0;
-}
-
-int RADIUS::Stop()
-{
-    printfd(__FILE__, "RADIUS::Stop()\n");
-    if (!isRunning)
-        return 0;
-
-    m_onAddUserConn.disconnect();
-    m_onDelUserConn.disconnect();
-
-    m_conns.clear();
-
-    isRunning = false;
-    return 0;
-}
-
-int RADIUS::SendMessage(const Message &, uint32_t) const
-{
-    errorStr = "Authorization modele \'Radius\' does not support sending messages";
-    return -1;
-}
-
