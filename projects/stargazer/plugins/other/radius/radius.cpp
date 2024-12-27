2f28e0760a1b58d0784458f3a0d9020491017187
Elena Mamontova <helenh463@gmail.com>
2024-12-27 15:22:50 +0200

    Radius. Header files added. Functions Start, Stop, Run definition added.

--- projects/stargazer/plugins/other/radius/radius.cpp
+++ projects/stargazer/plugins/other/radius/radius.cpp
@@ -1,4 +1,10 @@
 #include "radius.h"
+#include "server.h"
+#include "radproto/error.h"
+#include <boost/asio.hpp>
+#include <string>
+#include <iostream>
+#include <cstdint> //uint8_t, uint32_t
 
 using STG::RADIUS;
 
@@ -17,3 +23,34 @@ RADIUS::RADIUS()
 {
 }
 
+int RADIUS::Start()
+{
+    isRunning = true;
+    m_thread = std::jthread([this](){ Run(); });
+    return 0;
+}
+
+int RADIUS::Stop()
+{
+    std::lock_guard lock(m_mutex);
+    isRunning = false;
+    return 0;
+}
+
+int Run()
+{
+    std::string secret;
+    uint16_t port = 1812;
+
+    try
+    {
+        boost::asio::io_service io_service;
+        Server server(io_service, secret, port, "/usr/share/freeradius/dictionary");
+        io_service.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() <<"\n";
+    }
+    return 0;
+}
