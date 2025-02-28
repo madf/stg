f85212837364c04595d5cf9cd6ef201aaaea366a
Elena Mamontova <helenh463@gmail.com>
2025-02-28 14:37:05 +0200

    Radius. The methods declaration stop and start added.

--- projects/stargazer/plugins/other/radius/server.h
+++ projects/stargazer/plugins/other/radius/server.h
@@ -13,11 +13,12 @@ namespace STG
     {
         public:
             Server(boost::asio::io_service& io_service, const std::string& secret, uint16_t port, const std::string& filePath);
-
+            void stop();
         private:
             RadProto::Packet makeResponse(const RadProto::Packet& request);
             void handleReceive(const boost::system::error_code& error, const std::optional<RadProto::Packet>& packet, const boost::asio::ip::udp::endpoint& source);
             void handleSend(const boost::system::error_code& ec);
+            void start();
             void startReceive();
 
             RadProto::Socket m_radius;
