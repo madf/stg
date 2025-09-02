eb0110004ab3a620894397bde6745cad7c82e63f
Elena Mamontova <helenh463@gmail.com>
2025-09-02 14:28:37 +0300

    Radius. The settings.h header file changed to config.h.

--- projects/stargazer/plugins/other/radius/server.h
+++ projects/stargazer/plugins/other/radius/server.h
@@ -2,7 +2,7 @@
 
 #include "radproto/socket.h"
 #include "radproto/packet.h"
-#include "settings.h"
+#include "config.h"
 #include "radproto/dictionaries.h"
 #include "stg/logger.h"
 #include <boost/asio.hpp>
