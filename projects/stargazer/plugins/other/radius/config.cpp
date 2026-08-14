26431d17380fe44aeeeb1e9d0fe48eaab734a861
Elena Mamontova <helenh463@gmail.com>
2026-08-14 15:31:20 +0300

    The using ASectionData added. The parameter type of function ShowRules changed. The type of function ParseRules changed. Variables attrName,     attrCode, attrType definition added in the function ParseRules. Variables     m_dictionaries definition changed, m_dictionariesPath added in the     function ParseRules.

--- projects/stargazer/plugins/other/radius/config.cpp
+++ projects/stargazer/plugins/other/radius/config.cpp
@@ -13,10 +13,11 @@
 using STG::Config;
 using AttrValue = Config::AttrValue;
 using ASection = Config::ASection;
+using ASectionData = Config::ASectionData;
 
 namespace
 {
-    std::string ShowRules(const std::vector<std::pair<std::string, AttrValue>>& rules)
+    std::string ShowRules(const std::vector<ASectionData>& rules)
     {
         std::string result;
         for (const auto& at : rules)
@@ -24,23 +25,23 @@ namespace
             if (!result.empty())
                 result += ", ";
 
-            if (at.second.type == AttrValue::Type::PARAM_NAME)
-                result.append(at.first + " = " + at.second.value);
+            if (at.value.type == AttrValue::Type::PARAM_NAME)
+                result.append(at.attrName + " = " + at.value.value);
             else
-                result.append(at.first + " = '" + at.second.value + "'");
+                result.append(at.attrName + " = '" + at.value.value + "'");
         }
         return result;
     }
 }
 
-std::vector<std::pair<std::string, AttrValue>> Config::ParseRules(const std::string& value, const std::string& paramName)
+std::vector<ASectionData> Config::ParseRules(const std::string& value, const std::string& paramName)
 {
     using tokenizer = boost::tokenizer<boost::char_separator<char>>;
     const boost::char_separator<char> sep(",");
 
     const tokenizer tokens(value, sep);
 
-    std::vector<std::pair<std::string, AttrValue>> res;
+    std::vector<ASectionData> res;
 
     for (const auto& token : tokens)
     {
@@ -69,7 +70,12 @@ std::vector<std::pair<std::string, AttrValue>> Config::ParseRules(const std::str
             printfd(__FILE__, "Error ParseRules: '%s' attribute parameter value is invalid.\n", paramName.c_str());
             return {};
         }
-        res.emplace_back(keyValue[0], AttrValue{valueName, type});
+
+        const std::string attrName = keyValue[0];
+        const uint32_t attrCode = m_dictionaries.attributeCode(attrName);
+        const auto attrType = m_dictionaries.attributeType(attrCode);
+
+        res.push_back({attrName, attrCode, attrType, {valueName, type}});
     }
     return res;
 }
@@ -90,7 +96,8 @@ ASection Config::parseASection(const std::vector<ParamValue>& conf)
 
 Config::Config()
     : m_port(1812),
-      m_dictionaries("/usr/share/freeradius/dictionary"),
+      m_dictionariesPath("/usr/share/freeradius/dictionary"),
+      m_dictionaries(m_dictionariesPath),
       m_logger(PluginLogger::get("radius"))
 {}
 
@@ -126,7 +133,7 @@ int Config::ParseSettings(const ModuleSettings & s)
     pv.param = "Dictionaries";
     pvi = std::find(s.moduleParams.begin(), s.moduleParams.end(), pv);
     if (pvi != s.moduleParams.end() && !pvi->value.empty())
-        m_dictionaries = pvi->value[0];
+        m_dictionariesPath = pvi->value[0];
 
     const auto authIt = std::find(s.moduleParams.begin(), s.moduleParams.end(), ParamValue("auth", {}));
     if (authIt != s.moduleParams.end())
